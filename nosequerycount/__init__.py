daae86fac5d11399b4acbd8b0014d5773df5db23
Alexander Gor <ornot.work@gmail.com>
2017-06-01 18:48:29 +0300

    coma syntax is outdated

--- nosequerycount/__init__.py
+++ nosequerycount/__init__.py
@@ -5,5 +5,5 @@ Nose Django query count plugin
 try:
     VERSION = __import__('pkg_resources') \
         .get_distribution('django-sentry').version
-except Exception, e:
+except Exception as e:
     VERSION = 'unknown'
